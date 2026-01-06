af648e66986ed7cf768898a55abe32894cf03d27
aikonwu22-commits <aikonwu22@gmail.com>
2026-01-06 16:02:50 +0800

    cors 添加

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const cors = require('cors');const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js');
+const cors = require('cors');
+const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js');
 const qrcode = require('qrcode');
 const { Client, LocalAuth } = require('whatsapp-web.js');
 
